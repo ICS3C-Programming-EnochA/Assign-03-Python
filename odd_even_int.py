2872df74e8608c7217df6f1b30ce85ca4552b610
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-04-08 16:42:21 +0000

    odd_even_int.py

--- odd_even_int.py
+++ odd_even_int.py
@@ -5,20 +5,24 @@
 
 
 def main():
-
+    # Enter any number
     user_number = input("Enter any number: ")
     try:
         user_number = int(user_number)
+    # if the user_number is divisible by two and gives a zero it's an even number
         if user_number % 2 == 0:
             print("This number is an even number.")
 
         else:
+            # if the user_number is divisible by three and gives you a zero it's an odd number
             print("This number is an odd number.")
 
     except:
+        # if user_number is not a number the below code will display it is not a number
         print("This is not a number")
 
     finally:
+        # Thank you for playing
         print("Thank you and have a nice day")
 
 
